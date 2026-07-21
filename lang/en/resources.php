5f6802efb4469f6eb42e39a58e5c4cddaf13792a
Patrick Zuurbier <patrickzuurbier@gmail.com>
2026-07-21 15:50:43 +0200

    Move pages translations into package

--- lang/en/resources.php
+++ lang/en/resources.php
@@ -6,9 +6,34 @@ return [
     'pages' => [
         'singular' => 'Page',
         'plural'   => 'Pages',
+        'fields'   => [
+            'active'           => 'Active',
+            'intro'            => 'Intro',
+            'meta_description' => 'Meta description',
+            'meta_keywords'    => 'Meta keywords',
+            'meta_title'       => 'Meta title',
+            'name'             => 'Name',
+            'slug'             => 'Slug',
+            'title'            => 'Title',
+        ],
+        'tabs' => [
+            'paragraphs' => 'Paragraphs',
+        ],
     ],
     'paragraphs' => [
         'singular' => 'Paragraph',
         'plural'   => 'Paragraphs',
+        'fields'   => [
+            'active'  => 'Active',
+            'anchor'  => 'Anchor',
+            'content' => 'Content',
+            'intro'   => 'Intro',
+            'name'    => 'Name',
+            'order'   => 'Order',
+            'title'   => 'Title',
+        ],
+        'tabs' => [
+            'page' => 'Page',
+        ],
     ],
 ];
--- lang/nl/resources.php
+++ lang/nl/resources.php
@@ -6,9 +6,34 @@ return [
     'pages' => [
         'singular' => 'Pagina',
         'plural'   => 'Pagina\'s',
+        'fields'   => [
+            'active'           => 'Actief',
+            'intro'            => 'Intro',
+            'meta_description' => 'Meta omschrijving',
+            'meta_keywords'    => 'Meta zoekwoorden',
+            'meta_title'       => 'Meta titel',
+            'name'             => 'Naam',
+            'slug'             => 'Slug',
+            'title'            => 'Titel',
+        ],
+        'tabs' => [
+            'paragraphs' => 'Paragrafen',
+        ],
     ],
     'paragraphs' => [
         'singular' => 'Paragraaf',
         'plural'   => 'Paragrafen',
+        'fields'   => [
+            'active'  => 'Actief',
+            'anchor'  => 'Anchor',
+            'content' => 'Content',
+            'intro'   => 'Intro',
+            'name'    => 'Naam',
+            'order'   => 'Volgorde',
+            'title'   => 'Titel',
+        ],
+        'tabs' => [
+            'page' => 'Pagina',
+        ],
     ],
 ];
